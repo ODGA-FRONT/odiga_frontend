46bc399a35117b0a64ff919f59bbbd1e44b2c3df
이주이 <dea8307@gmail.com>
2024-07-09 22:32:53 +0900

    fix : 현재위치 계속해서 갱신 ( 리팩토링 필요함 )

--- src/app/(route)/map/_components/mapSection.tsx
+++ src/app/(route)/map/_components/mapSection.tsx
@@ -1,20 +1,52 @@
 'use client';
 
 import Map from '@/components/map';
-import { useCurrentLocation } from '@/hooks/useCurrentLocation';
 import { Coordinates, NaverMap } from '@/types/map';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export const INITIAL_CENTER: Coordinates = [37.5666103, 126.9783882];
 export const INITIAL_ZOOM = 15;
 
-const MapSection = () => {
-  const { location, refreshLocation } = useCurrentLocation();
+const options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0,
+};
 
+const MapSection = () => {
   const mapRef = useRef<NaverMap | null>(null);
   const markerRef = useRef<naver.maps.Marker | null>(null);
+  const currentLocationRef = useRef<Coordinates>();
+  const [error, setError] = useState('');
+
+  const handleSuccess = (pos: GeolocationPosition) => {
+    const { latitude, longitude } = pos.coords;
+    const newLocation: Coordinates = [latitude, longitude];
+    currentLocationRef.current = newLocation;
+    addMarkerMap(currentLocationRef.current);
+  };
+
+  const handleError = (err: GeolocationPositionError) => {
+    currentLocationRef.current = [...INITIAL_CENTER];
+    setError('Unable to retrieve location.');
+  };
+
+  // 현재위치 갱신 함수
+  const refreshLocation = () => {
+    const { geolocation } = navigator;
+
+    if (!geolocation) {
+      setError('Geolocation is not supported.');
+      return;
+    }
+    setInterval(() => {
+      geolocation.getCurrentPosition(handleSuccess, handleError, options);
+    }, 1000);
+  };
 
   const initializeMap = () => {
+    const { geolocation } = navigator;
+    // 최초 실행함수
     const mapOptions = {
       center: new naver.maps.LatLng(...INITIAL_CENTER),
       logoControl: false, // 네이버 로고 표시 X
@@ -29,10 +61,41 @@ const MapSection = () => {
     // 새로운 네이버 맵 인스턴스 생성
     const map = new window.naver.maps.Map('map', mapOptions);
     mapRef.current = map;
+
+    if (geolocation) {
+      const handleSuccess = (pos: GeolocationPosition) => {
+        const { latitude, longitude } = pos.coords;
+        const newLocation: Coordinates = [latitude, longitude];
+
+        if (map) {
+          const newPosition = new naver.maps.LatLng(...newLocation);
+          if (!markerRef.current) {
+            const marker = new naver.maps.Marker({
+              position: newPosition,
+              map: map,
+              zIndex: 999,
+            });
+            markerRef.current = marker;
+          } else {
+            markerRef.current.setPosition(newPosition);
+          }
+          map.setCenter(newPosition);
+        }
+      };
+
+      geolocation.getCurrentPosition(
+        handleSuccess,
+        () => {
+          console.log('err');
+        },
+        options,
+      );
+    }
   };
 
-  const addMarkerAndCenterMap = () => {
-    if (location && mapRef.current) {
+  const addMarkerMap = (location: Coordinates) => {
+    // marker를 map에 새로 만드는 함수
+    if (mapRef.current) {
       const newPosition = new naver.maps.LatLng(...location);
 
       if (!markerRef.current) {
@@ -45,17 +108,24 @@ const MapSection = () => {
       } else {
         markerRef.current.setPosition(newPosition);
       }
+      return newPosition;
+    }
+  };
 
-      mapRef.current.setCenter(newPosition);
+  const handleCurrentLocation = () => {
+    // 현재위치 눌러졌을 때, 호출되는 함수
+    // 위치 바꾸고, 중간으로 보냄
+    if (currentLocationRef.current) {
+      const newPosition = addMarkerMap(currentLocationRef.current);
+      if (mapRef.current && newPosition) {
+        mapRef.current.setCenter(newPosition);
+      }
     }
   };
 
   useEffect(() => {
-    if (location) {
-      // 현재위치 파악되면 다시 위치에 맞게끔 지도 그림
-      addMarkerAndCenterMap();
-    }
-  }, [location]);
+    refreshLocation();
+  }, []);
 
   useEffect(() => {
     return () => {
@@ -64,7 +134,7 @@ const MapSection = () => {
     };
   }, []);
 
-  return <Map initializeMap={initializeMap} handleCurrentLocation={refreshLocation} />;
+  return <Map initializeMap={initializeMap} handleCurrentLocation={handleCurrentLocation} />;
 };
 
 export default MapSection;
--- src/hooks/useCurrentLocation.ts
+++ src/hooks/useCurrentLocation.ts
@@ -1,50 +1,5 @@
-import { INITIAL_CENTER } from '@/app/(route)/map/_components/mapSection';
-import { Coordinates } from '@/types/map';
-import { useEffect, useState } from 'react';
-
 const LOCAL_STORAGE_KEY = 'currentLocation';
 
 export const useCurrentLocation = () => {
-  const [location, setLocation] = useState<Coordinates>(() => {
-    const cachedLocation = typeof window !== 'undefined' && localStorage.getItem(LOCAL_STORAGE_KEY);
-    return cachedLocation ? JSON.parse(cachedLocation) : INITIAL_CENTER;
-  });
-  const [error, setError] = useState('');
-
-  const handleSuccess = (pos: GeolocationPosition) => {
-    const { latitude, longitude } = pos.coords;
-    const newLocation: Coordinates = [latitude, longitude];
-
-    setLocation(newLocation);
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newLocation));
-  };
-
-  const handleError = (err: GeolocationPositionError) => {
-    setLocation([...INITIAL_CENTER]);
-    setError('Unable to retrieve location.');
-  };
-
-  // 현재위치 갱신 함수
-  const refreshLocation = () => {
-    const { geolocation } = navigator;
-
-    if (!geolocation) {
-      setError('Geolocation is not supported.');
-      return;
-    }
-
-    const options = {
-      enableHighAccuracy: true,
-      timeout: 5000,
-      maximumAge: 0,
-    };
-
-    geolocation.getCurrentPosition(handleSuccess, handleError, options);
-  };
-
-  useEffect(() => {
-    refreshLocation();
-  }, []);
-
-  return { location, error, refreshLocation };
+  return {};
 };
